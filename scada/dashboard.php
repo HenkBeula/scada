f50d4dd449b2245fcfa84385096556ebd54a23c3
Henk Beula <henkbeula82@gmail.com>
2021-12-09 23:11:36 +0200

    changed passee to passe

--- scada/dashboard.php
+++ scada/dashboard.php
@@ -86,7 +86,6 @@
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group me-2">
               <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
         </div>
       </div>
     </div>
